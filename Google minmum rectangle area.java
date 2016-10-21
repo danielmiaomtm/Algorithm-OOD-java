790dbc864d64774ec9d002ab3ff6372bd5a7221b
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-10-21 13:09:57 -0700

    Update Google minmum rectangle area.java

--- Google minmum rectangle area.java
+++ Google minmum rectangle area.java
@@ -9,4 +9,37 @@
   2.2 查找set中是否存在（x1, y2）， （x2, y1）. 若都存在，计算面积并更新最小面积
 3. 输出最小面积。
 */
-
+class Point{
+  private int x;
+  private int y;
+  public Point (int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+public int minRectArea (Point[] points) {
+  int result = 0;
+  Set<Point> set = new HashSet<>();
+  for (Point point : points) {
+    set.add(point);
+  }
+  List<Point> list = new ArrayList<>();
+  for (Point p : set) {
+    list.add(p);
+  }
+  for (int i = 0; i < list.size(); i++) {
+    Point leftBottom = list.get(i);
+    for (int j = i; j < list.size(); j++) {
+      Point rightUpper = list.geT(j);
+      //leftUpper
+      Point leftUpper = new Point (rightUpper.x, leftButtom.y);
+      //rightBottom
+      Point rightBottom = new Point(leftButtom.x, rightUpper.y);
+      if (set.contains(leftUpper) &&& set.contains(rightBottom)) {
+        int max = (rightButtom.y - leftUpper.y) * (leftUpper.x - rightButtom.x);
+        result = Math.max(result, max);
+      }
+    }
+  }
+  return result;
+}

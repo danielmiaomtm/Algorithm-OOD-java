3627f8762f81a04dc98ae94b60b3f7f6b76671b3
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-10-31 20:13:08 -0700

    Update and rename Diagnostically print matrix to Diagnostically print matrix.java

--- Diagnostically print matrix.java
+++ Diagnostically print matrix.java
@@ -1,3 +1,8 @@
+[1,2,3]
+[4,5,6]
+[7,8,9]
+    
+1 | 4 2 | 7 5 3 | 8 6 | 9    
     public void helper (int[][] nums) {
         int rowCount = nums.length;
         int columnCount = nums[0].length;
@@ -21,3 +26,22 @@
         
     }
     
+7 | 4 8 | 1 5 9 | 2 6 | 3
+
+public void helper (int[][] nums) {
+    int rowNum = nums.length;
+    int colNum = nums[0].length;
+    
+    for (int i = row - 1; i >= 0; i--) {
+        List<Integer> list = new ArrayList<>();
+        for (int row = i && col = 0; row < rowNum && col < colNum; row--, col++) {
+            list.add(nums[row][col]);
+        }
+    }
+    for (int j = 1; j < colNum; j++) {
+        List<Integer> list = new ArrayList<>();
+        for (int row = 0, col = i; row < rowNum && col < colNum; row++, col++) {
+            list.add(nums[row][col]);
+        }
+    }
+}

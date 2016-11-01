23104529df181da05bb67143ae4e322ff0b5a038
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-10-31 22:54:08 -0700

    Create Google Perfect Squares平方和.java

---
+++ Google Perfect Squares平方和.java
@@ -0,0 +1,21 @@
+/*
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+*/
+
+
+public class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; i + j * j <= n; j++) {
+                dp[i + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+            }
+        }
+        return dp[n];
+    }
+}

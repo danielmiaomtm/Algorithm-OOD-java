77c69e00e221d0e84f2a02fe2506eef960db0104
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-11-08 13:37:10 -0800

    Update OOD file system.java

--- OOD file system.java
+++ OOD file system.java
@@ -1,60 +1,83 @@
-package filesystem;
+    abstract class Entry {
+        protected Directory parent = null;
+        protected long created;
+        protected long lastUpdated;
+        protected long lastAccessed;
+        protected String name;
 
-import java.util.ArrayList;
-import java.util.Date;
-
-public class FileSystem {
-
-    public class file {
-        private String name;
-        private long size;
-        private Date timeStamp;
-        private file currentDir;
-        private file parentDir;
-
-        // a directory is also a file containing reference to other files
-        private boolean isDirectory;
-        public ArrayList<file> subfiles;
-
-        // Advanced class members if required
-        private boolean[] permissions;
-        private String owner;
-        private String group;
-           
-        public file(String name, file currentDir, boolean isDir) {
+        public Entry (String name, Directory parent) {
             this.name = name;
-            this.currentDir = currentDir;
-            this.timeStamp = new Date();
-            this.isDirectory = isDir;
-            this.size = 0; // initial size
-            this.parentDir = currentDir.getParentDirectory();
-            if (isDir == true)
-                this.subfiles = new ArrayList<file>();
+            this.parent = parent;
+            this.created = System.currentTimeMillis();
         }
-        
-        public void updateTimeStamp() {
-            this.timeStamp = new Date();
-        }
-        
-        public file getParentDirectory() {
-            return this.parentDir;
-        }
-        
-        public void rename(String name) {
-            this.name = name;
-        }
-    
-    }
-    
-    private file root; // root folder
 
-    public FileSystem() {
-        // every file system should have a root folder
-        this.root = new file("root", null, true);
+        public boolean delete () {
+            if (parent == null) {
+                return false;
+            }
+            return parent.deletEntry(this);
+        }
+
+        public String getFullPath () {
+            if (parent == null) {
+                return name;
+            } 
+            return parent.getFullPath() + "/" + name;
+        }
+        public abstract int size();
     }
-    
-    public void createFile(String name, file curDir, boolean isDir) {
-        file f = new file(name, curDir, isDir);
-        curDir.subfiles.add(f);
+
+    class File extends Entry {
+        private String content;
+        private int size;
+        public File (String name, Directory parent, int size, String content) {
+            super(name, parent);
+            size = size;
+            content = content;
+        }
+        public int size () {
+            return this.size;
+        }
+        public String getContent () {
+            return this.content;
+        }
     }
-}
+
+    class Directory extends Entry {
+
+        protected List<Entry> contents;
+
+        public Directory (String name, Directory parent) {
+            super (name, parent);
+            contents = new ArrayList<>();
+        }
+        protected List<Entry> getContents () {
+            return contents;
+        }
+        public int size() {
+            int size = 0;
+            for (Entry e : contents) {
+                size += e.size();
+            }
+            return size;
+        }
+        public int numOfFiles () {
+            int count = 0;
+            for (Entry e : contents) {
+                if (e instanceof Directory) {
+                    count++;
+                    Directory d = (Directory) e;
+                    count += d.numOfFiles();
+                } else if (e instanceof File) {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public void deleteEntry(Entry entry) {
+            return contents.remove(entry);
+        }
+        public void addEntry (Entry entry) {
+            contents.add(entry);
+        }
+    }

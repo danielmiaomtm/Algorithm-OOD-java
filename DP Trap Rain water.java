89f50a05751a767db6a4ccfa58ac6f8d7fb1ca8b
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-11-09 09:57:57 -0800

    Update and rename trap rain water to DP Trap Rain water.java

--- DP Trap Rain water.java
+++ DP Trap Rain water.java
@@ -1,3 +1,15 @@
+/*
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it is able to trap after raining.
+
+Trapping Rain Water
+
+Have you met this question in a real interview? Yes
+Example
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+
+*/
+
 public int trapRainWater(int[] heights) {
         // look from right to left
         if (heights.length <= 2 || heights == null) {

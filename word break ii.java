1790bdff4826fbed18fcedd4aae154e9f4e3e3a2
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-11-08 16:57:05 -0800

    Update and rename word break ii to word break ii.java

--- word break ii.java
+++ word break ii.java
@@ -1,4 +1,42 @@
 /*
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one 
+or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+*/
+
+    public class Solution {
+        public boolean wordBreak(String s, Set<String> wordDict) {
+            int maxWord = getMax(wordDict);
+            int len = s.length();
+            boolean[] dp = new boolean[len + 1];
+            dp[0] = true;
+            for (int i = 1; i <= len; i ++) {
+                int start = Math.max(1, i - maxWord);
+                for (int j = start; j <= i; j++) {
+                    if (dp[j - 1] && wordDict.contains(s.substring(j - 1, i))) {
+                        dp[i] = true; 
+                        break;
+                    }
+                }
+            }
+            return dp[len];
+        }
+
+        private int getMax(Set<String> wordDict) {
+            int max = 0;
+            for (String str : wordDict) {
+                max = Math.max(max, str.length());
+            }
+            return max;
+        }
+    }
+
+/*
 Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
 
 Return all such possible sentences.

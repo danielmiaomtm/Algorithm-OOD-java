655808a94584586d1fa1e2f3900813e91d9708c4
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-11-17 17:28:59 -0800

    Update Inorder Successor in BST.java

--- Inorder Successor in BST.java
+++ Inorder Successor in BST.java
@@ -1,29 +1,31 @@
+
+//lgn time
 public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-	TreeNode node = root;
-	Stack<TreeNode> stack = new Stack<>();
+    if(root==null)
+	return null;
 
-	while (node != p) {
-		stack.push(node.val);
-		if (node.val < p.val) {
-			node = node.right;
-		} else {
-			node = node.left;
-		}
+    TreeNode next = null;
+    TreeNode c = root;
+    while(c!=null && c.val!=p.val){
+	if(c.val > p.val){
+	    next = c;
+	    c = c.left;
+	}else{
+	    c= c.right;
 	}
-	if (node.right != null) {
-		node = node.right;
-	
-		while (node.left != null) {
-			node = node.left;
-		}
-		return node;
-	} else {
-		while (!stack.isEmpty() && stack.peek().val < node.val) {
-			stack.pop();
-		}
-		return stack.isEmpty() ? null : stack.pop();
-	}
+    }
 
+    if(c==null)        
+	return null;
+
+    if(c.right==null)
+	return next;
+
+    c = c.right;
+    while(c.left!=null)
+	c = c.left;
+
+    return c;
 }
 
 

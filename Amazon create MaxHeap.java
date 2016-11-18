166020f9b5742e9edc1612d2994613a7bfaac32e
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-11-17 20:49:32 -0800

    Update Amazon create MaxHeap.java

--- Amazon create MaxHeap.java
+++ Amazon create MaxHeap.java
@@ -1,126 +1,83 @@
-// Heap.java
+/* By Vamei 
+   Use an big array to implement heap
+   DECLARE: int heap[MAXSIZE] in calling function
+   heap[0] : total nodes in the heap
+   for a node i, its children are i*2 and i*2+1 (if exists)
+   its parent is i/2  */
 
-import java.io.Serializable;
-
-public class Heap implements Serializable{
-    private int heapLength;
-    private int [] data;
-
-    public int getHeapLength() {
-        return heapLength;
-    }
-
-    public void setHeapLength(int heapLength) {
-        this.heapLength = heapLength;
-    }
-
-    public int[] getData() {
-        return data;
-    }
-
-    public void setData(int[] data) {
-        this.data = data;
-    }
+void insert(int new, int heap[]) 
+{
+    int childIdx, parentIdx;
+    heap[0] = heap[0] + 1;
+    heap[heap[0]] = new;
+    
+    /* recover heap property */
+    percolate_up(heap);
 }
 
-//Heapsort.java
-
-/**
- * Created with IntelliJ IDEA.
- * Date: 2014/8/17
- * Time: 15:39
- */
-public class HeapSort {
-    public final static int getLeft(int i) {
-        return i << 1;
-    }
-
-    public final static int getRight(int i) {
-        return (i << 1) + 1;
-    }
-
-    public final static int getParent(int i) {
-        return i >> 1;
-    }
-
-    /**
-     * 保持堆的性质
-     *
-     * @param heap
-     * @param i
-     */
-    public static void maxHeapify(Heap heap, int i) {
-        if (null == heap || null == heap.getData() || heap.getData().length <= 0 || i < 0)
-            return;
-        int l = getLeft(i);
-        int r = getRight(i);
-        int largest = 0;
-        if (l < heap.getHeapLength() && heap.getData()[l] > heap.getData()[i])
-            largest = l;
-        else
-            largest = i;
-        if (r < heap.getHeapLength() && heap.getData()[r] > heap.getData()[largest])
-            largest = r;
-        if (largest != i) {
-            int tmp = heap.getData()[i];
-            heap.getData()[i] = heap.getData()[largest];
-            heap.getData()[largest] = tmp;
-            maxHeapify(heap, largest);
-        }
-    }
-
-    /**
-     * 建立最大堆
-     *
-     * @param array
-     * @return
-     */
-    public static Heap bulidMaxHeap(int[] array) {
-        if (null == array || array.length <= 0)
-            return null;
-        Heap heap = new Heap();
-        heap.setData(array);
-        heap.setHeapLength(array.length);
-        for (int i = (array.length >> 1); i >= 0; i--)
-            maxHeapify(heap, i);
-        return heap;
-    }
-
-    /**
-     * 堆排序
-     *
-     * @param array
-     */
-    public static void heapSort(int[] array) {
-        if (null == array || array.length <= 0)
-            return;
-        Heap heap = bulidMaxHeap(array);
-        if (null == heap)
-            return;
-        for (int i = heap.getHeapLength() - 1; i > 0; i--) {
-            int tmp = heap.getData()[0];
-            heap.getData()[0] = heap.getData()[i];
-            heap.getData()[i] = tmp;
-            heap.setHeapLength(heap.getHeapLength() - 1);
-            maxHeapify(heap, 0);
-        }
+static void percolate_up(int heap[]) {
+    int lightIdx, parentIdx;
+    lightIdx  = heap[0];
+    parentIdx = lightIdx/2;
+    /* lightIdx is root? && swap? */
+    while((parentIdx > 0) && (heap[lightIdx] < heap[parentIdx])) {
+        /* swap */
+        swap(heap + lightIdx, heap + parentIdx); 
+        lightIdx  = parentIdx;
+        parentIdx = lightIdx/2;
     }
 }
 
 
-//测试
-
-public class Main {
-
-    public static void main(String[] args) {
-        int a[] = {9, 3, 4, 1, 5, 10, 7};
-        System.out.println("Hello World!");
-        Sort sort = new Sort();
-//        sort.bubbleSort(a);
-//        sort.selectSort(a);
-//        sort.insertionSort(a);
-        HeapSort.heapSort(a);
-        for (int i = 0; i < a.length; i++)
-            System.out.println(a[i]);
+int delete_min(int heap[]) 
+{
+    int min;
+    if (heap[0] < 1) {
+        /* delete element from an empty heap */
+        printf("Error: delete_min from an empty heap.");
+        exit(1);
     }
+
+    /* delete root 
+       move the last leaf to the root */
+    min = heap[1];
+    swap(heap + 1, heap + heap[0]);
+    heap[0] -= 1;
+
+    /* recover heap property */
+    percolate_down(heap);
+ 
+    return min;
+}
+
+static void percolate_down(int heap[]) {
+    int heavyIdx;
+    int childIdx1, childIdx2, minIdx;
+    int sign; /* state variable, 1: swap; 0: no swap */
+
+    heavyIdx = 1;
+    do {
+        sign     = 0;
+        childIdx1 = heavyIdx*2;
+        childIdx2 = childIdx1 + 1;
+        if (childIdx1 > heap[0]) {
+            /* both children are null */
+            break; 
+        }
+        else if (childIdx2 > heap[0]) {
+            /* right children is null */
+            minIdx = childIdx1;
+        }
+        else {
+            minIdx = (heap[childIdx1] < heap[childIdx2]) ?
+                          childIdx1 : childIdx2;
+        }
+
+        if (heap[heavyIdx] > heap[minIdx]) {
+            /* swap with child */
+            swap(heap + heavyIdx, heap + minIdx);
+            heavyIdx = minIdx;
+            sign = 1;
+        }
+    } while(sign == 1);
 }

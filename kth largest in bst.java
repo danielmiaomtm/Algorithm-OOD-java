6f2e2f995a5765581607448c8641037f076cbe99
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-11-19 17:51:58 -0800

    Update kth largest in bst.java

--- kth largest in bst.java
+++ kth largest in bst.java
@@ -23,3 +23,27 @@
     }
     helper(root.left, k, count, result);
   }
+
+
+
+// kth smallest in BST
+public int kthSmallest(TreeNode root, int k) {
+       if (root == null) {
+            return 0;
+        }
+        int size = findSize(root.left);
+        if (k == size + 1) {
+            return root.val;
+        } else if (k > size + 1) {
+            return kthSmallest(root.right, k - size - 1);
+        } else {
+            return kthSmallest(root.left, k);
+        }
+        
+    }
+    public int findSize (TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + findSize(root.left) + findSize(root.right);
+    }

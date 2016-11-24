2ace3856781431e931166b5f98173cda40055961
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-11-24 09:22:08 -0800

    Update Microsoft Rearrange String k Distance Apart.java

--- Microsoft Rearrange String k Distance Apart.java
+++ Microsoft Rearrange String k Distance Apart.java
@@ -10,6 +10,47 @@ Result: "abcabc"
 
 The same letters are at least distance 3 from each other.
 
+    
+//O(n) Time
+public String rearrangeString(String str, int k) {
+    if (k > 26) {
+        return "";
+    }
+    int length = str.length();
+    int[] count = new int[26];
+    int[] valid = new int[26];
+    for(int i=0;i<length;i++){
+        count[str.charAt(i)-'a']++;
+    }
+    StringBuilder sb = new StringBuilder();
+    for(int index = 0;index<length;index++){
+        int candidatePos = findValidMax(count, valid, index);
+        if( candidatePos == -1) return "";
+        count[candidatePos]--;
+        valid[candidatePos] = index+k;
+        sb.append((char)('a'+candidatePos));
+    }
+    return sb.toString();
+}
+
+private int findValidMax(int[] count, int[] valid, int index){
+   int max = Integer.MIN_VALUE;
+   int candidatePos = -1;
+   for(int i=0;i<count.length;i++){
+       if(count[i]>0 && count[i]>max && index>=valid[i]){
+           max = count[i];
+           candidatePos = i;
+       }
+   }
+   return candidatePos;
+}
+
+
+
+
+
+    
+//nlgn Time    
 
 public String rearrangeString(String str, int k) {
     if(k==0)

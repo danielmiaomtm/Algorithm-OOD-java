0529faae090708681025418351ea40a398c9cd6d
danielmiaomtm <jeffreymaomtm@gmail.com>
2016-12-29 23:28:56 -0800

    Update Google find longest subsequence in wordDict.java

--- Google find longest subsequence in wordDict.java
+++ Google find longest subsequence in wordDict.java
@@ -48,3 +48,78 @@ public int maxSubsequence (List<String> dict, String target) {
 	
 	return maxLen;
 }
+
+
+
+
+
+class TrieNode {
+	TrieNode[] children;
+	int val;
+	boolean isWord;
+	TrieNode (int val) {
+		this.val = val;
+		this.children = new TrieNode[26];
+		this.isWord = false;
+	}
+}
+int[][] arrs;
+public int longestSubsequence (String target, List<String> dict) {
+	int len = target.length();
+	if (len == 0) {
+		return 0;
+	}
+	//build Trie Tree
+	TrieNode root = buildTrie(dict, len);
+	//generate pos map index : time: o(len of target)
+	this.arrs = new int[target.length()][26];
+	int maxLen = 0;
+
+	for (int i = target.length() - 1; i >= 0; i--) {
+		for (int j = 0; j < 26; j++) {
+			if (target.charAt(i) - 'a' == j) {
+				arrs[i][j] = i;	
+			} else {
+				if (i + 1 < target.length()) {
+					arrs[i][j] == arrs[i + 1][j];
+				} else {
+					arrs[i][j] = -1;
+				}
+			}
+		}
+	}
+
+	int maxLen = 0;
+	helper (root, 0, 0, maxLen);
+	return maxLen;
+
+}
+public void helper (TrieNode root, int start, int len, int maxLen) {
+	if (root == null || start == -1) {
+		return;
+	}
+	if (root.isWord) {
+		maxLen = Math.max(maxLen, len);
+	}
+	for (char c = 'a'; c <= 'z'; c++) {
+		helper (root.children[c - 'a'], arrs[start][c], len + 1, maxLen);
+	}
+}
+public TreeNode buildTrie (List<String> dict, int maxDepth) {
+	TreeNode result = new Node(-1);
+	TreeNode node = result;
+	for (String word : dict) {
+		if (word.length() > maxDepth) {
+			continue;
+		}
+		for (char c : word.toCharArray()) {
+			if (node.children[c - 'a'] == null) {
+				node.children[c - 'a'] = new TrieNode(c);
+			}
+			node = node.children[c - 'a'];
+		}
+		node.isWord = true;
+	}
+	return result;
+}
+

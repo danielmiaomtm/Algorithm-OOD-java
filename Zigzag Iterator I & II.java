cd13cd2b15b8ab1cafae0a685266c7c7197a0117
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-03 17:53:39 -0800

    Update and rename Zigzag Iterator I & II to Zigzag Iterator I & II.java

--- Zigzag Iterator I & II.java
+++ Zigzag Iterator I & II.java
@@ -1,3 +1,4 @@
+/*
 Given two 1d vectors, implement an iterator to return their elements alternately.
 
 For example, given two 1d vectors:
@@ -5,6 +6,7 @@ For example, given two 1d vectors:
 v1 = [1, 2]
 v2 = [3, 4, 5, 6]
 By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 3, 2, 4, 5, 6].
+*/
 
 public class ZigzagIterator {
 	int cur;
@@ -33,7 +35,7 @@ public class ZigzagIterator {
 }
 
 
-/////
+/*
 
 Follow up: What if you are given k 1d vectors? How well can your code be extended to such cases?
 
@@ -43,7 +45,7 @@ Clarification for the follow up question - Update (2015-09-18): The "Zigzag" ord
 [4,5,6,7]
 [8,9]
 It should return [1,4,8,2,5,9,3,6,7].
-
+*/
 
 
 public class ZigzagIterator implements Iterator<Integer> {

8f9e1795d0d5875730cda0040c486916c3a862c8
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-04 20:32:00 -0800

    Update Google Evaluate Division.java

--- Google Evaluate Division.java
+++ Google Evaluate Division.java
@@ -21,29 +21,27 @@ queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
 public class Solution {
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
         double[] result = new double[queries.length];
-        // filter unexpected words
-        // 过滤掉没有遇见过的字符
+
         Set<String> words = new HashSet<>();
         for (String[] strs : equations) {
             words.add(strs[0]);
             words.add(strs[1]);
         }
-        for (int i = 0; i < queries.length; ++i) {
+
+        for (int i = 0; i < queries.length; i++) {
             String[] keys = queries[i];
             if (!words.contains(keys[0]) || !words.contains(keys[1])) {
                 result[i] = -1.0d;
             } else {
-                Stack<Integer> stack = new Stack<>();
-                result[i] = helper(equations, values, keys, stack);
+                //stack to keep track of visited index
+                Set<Integer> set = new HashSet<>();
+                result[i] = helper(equations, values, keys, set);
             }
         }
         return result;
     }
-
-    public double helper(String[][] equations, double[] values, String[] keys, Stack<Integer> stack) {
-        // 直接查找，key的顺序有正反
-        // look up equations directly
-        for (int i = 0; i < equations.length; ++i) {
+    public double helper(String[][] equations, double[] values, String[] keys, Set<Integer> set) {
+        for (int i = 0; i < equations.length; i++) {
             if (equations[i][0].equals(keys[0]) && equations[i][1].equals(keys[1])) {
                 return values[i];
             }
@@ -51,26 +49,26 @@ public class Solution {
                 return 1 / values[i];
             }
         }
-        // lookup equations by other equations
-        // 间接查找，key的顺序也有正反
-        for (int i = 0; i < equations.length; ++i) {
-            if (!stack.contains(i) && keys[0].equals(equations[i][0])) {
-                stack.push(i);
-                double temp = values[i] * helper(equations, values, new String[]{equations[i][1], keys[1]}, stack);
-                if (temp > 0) return temp;
-                else {
-                    stack.pop();
+        for (int i = 0; i < equations.length; i++) {
+            if (!set.contains(i) && keys[0].equals(equations[i][0])) {
+                set.add(i);
+                double temp = values[i] * helper(equations, values, new String[]{equations[i][1], keys[1]}, set);
+                if (temp > 0) {
+                    return temp;
+                } else {
+                    set.remove(new Integer(i));
                 }
             }
-            if (!stack.contains(i) && keys[0].equals(equations[i][1])) {
-                stack.push(i);
-                double temp = helper(equations, values, new String[]{equations[i][0], keys[1]}, stack) / values[i];
-                if (temp > 0) return temp;
-                else stack.pop();
+            if (!set.contains(i) && keys[0].equals(equations[i][1])) {
+                set.add(i);
+                double temp = helper(equations, values, new String[]{equations[i][0], keys[1]}, set) / values[i];
+                if (temp > 0) {
+                    return temp; 
+                } else {
+                    set.remove(new Integer(i));
+                }
             }
         }
-        // 查不到，返回-1
         return -1.0d;
     }
-
 }

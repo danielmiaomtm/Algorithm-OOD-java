60c48c718bc426489a4786c5316ad77f2fd7b36d
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-16 18:20:15 -0800

    Update Facebook Remove Invalid Parentheses.java

--- Facebook Remove Invalid Parentheses.java
+++ Facebook Remove Invalid Parentheses.java
@@ -11,33 +11,59 @@ Examples:
 */
 
 
-public class Solution {
-    public List<String> removeInvalidParentheses(String s) {
-        List<String> result = new ArrayList<>();
-        char[] par = new char[]{'(', ')'};
-        helper(result, s, 0, 0, par);
-        return result;
+public List<String> removeInvalidParentheses(String s) {
+    List<String> res = new ArrayList<>();
+
+  // sanity check
+  if (s == null) return res;
+
+  Set<String> visited = new HashSet<>();
+  Queue<String> queue = new LinkedList<>();
+
+  // initialize
+  queue.add(s);
+  visited.add(s);
+
+  boolean found = false;
+
+  while (!queue.isEmpty()) {
+    s = queue.poll();
+
+    if (isValid(s)) {
+      // found an answer, add to the result
+      res.add(s);
+      found = true;
     }
-    public void helper (List<String> result, String s, int endi, int endj, char[] par) {
-        int stack = 0;
-        for (int i = endi; i < s.length(); i++) {
-            if (s.charAt(i) == par[0]) stack++;
-            if (s.charAt(i) == par[1]) stack--;
-            if (stack >= 0) continue;
-            // when there is additional ')' occur, so there could be possible solution (0, i)
-            for (int j = endj; j <= i; j++) {
-                if (s.charAt(j) == par[1] 
-                    && (j == endj || s.charAt(j - 1) != par[1])) {
-                    helper(result, s.substring(0, j) + s.substring(j + 1, s.length()), i, j, par);
-                }
-            }
-            return;
-        }
-        String reversed = new StringBuilder(s).reverse().toString();
-        if (par[0] == '(') {
-            helper(result, reversed, 0, 0, new char[]{')', '('});
-        } else {
-            result.add(reversed);
-        }
+
+    if (found) continue;
+
+    // generate all possible states
+    for (int i = 0; i < s.length(); i++) {
+      // we only try to remove left or right paren
+      if (s.charAt(i) != '(' && s.charAt(i) != ')') continue;
+
+      String t = s.substring(0, i) + s.substring(i + 1);
+
+      if (!visited.contains(t)) {
+        // for each state, if it's not visited, add it to the queue
+        queue.add(t);
+        visited.add(t);
+      }
     }
+  }
+
+  return res;
+}
+
+// helper function checks if string s contains valid parantheses
+boolean isValid(String s) {
+  int count = 0;
+
+  for (int i = 0; i < s.length(); i++) {
+    char c = s.charAt(i);
+    if (c == '(') count++;
+    if (c == ')' && count-- == 0) return false;
+  }
+
+  return count == 0;
 }

7cb9629d6dc2bccfd4700b5bc9c99d938b9acc28
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-25 16:36:15 -0800

    Create LinkedIn Nested List iterator.java

---
+++ LinkedIn Nested List iterator.java
@@ -0,0 +1,55 @@
+/*
+nested array，etc [[1,2], 2, [[3], [4]]]，input是nested array的iterator，实现next element的iterator
+*/
+
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class NestedIterator implements Iterator<Integer> {
+    
+    Stack<NestedInteger> stack = new Stack<>();
+    
+    public NestedIterator(List<NestedInteger> nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }    
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            return null;
+        }
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            NestedInteger cur = stack.peek();
+            if (cur.isInteger()) {
+                return true;
+            }
+            stack.pop();
+            for (int i = cur.getList().size() - 1; i >= 0; i--) {
+                stack.push(cur.getList().get(i));
+            }
+        }
+        return false;
+    }
+}

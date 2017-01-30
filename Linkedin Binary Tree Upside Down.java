de6b874d7febcb9e53005a53f552f08c81b56406
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-29 22:13:37 -0800

    Update Linkedin Binary Tree Upside Down.java

--- Linkedin Binary Tree Upside Down.java
+++ Linkedin Binary Tree Upside Down.java
@@ -19,6 +19,24 @@ confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on
 */
 
 
+
+
+public TreeNode upsideDownBinaryTree(TreeNode root) {
+    TreeNode node = root, parent = null, right = null;  
+    while (node != null) {  
+        TreeNode left = node.left;  
+        node.left = right;  
+        right = node.right;  
+        node.right = parent;  
+        parent = node;  
+        node = left;  
+    }  
+    return parent;  
+}
+
+
+
+
 public class Solution {
     public TreeNode upsideDownBinaryTree(TreeNode root) {
         if (root == null || (root.left == null && root.right == null)) {

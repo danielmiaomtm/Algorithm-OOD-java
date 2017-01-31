dd9be8e37595272a8047c29fc1572a1888588c1d
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-31 15:52:26 -0800

    Update KMP substring.java

--- KMP substring.java
+++ KMP substring.java
@@ -44,3 +44,41 @@ private int[] computeTemporaryArray(char pattern[]){
         }
         return false;
     }
+
+
+void KMPSearch(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+ 
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int lps[];
+        int j = 0;  // index for pat[]
+ 
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        int lps[] = computeTemporaryArray(pattern);
+ 
+        int i = 0;  // index for txt[]
+        while (i < N)
+        {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                System.out.println("Found pattern "+
+                              "at index " + (i-j));
+                    //replace string here
+                j = lps[j-1];
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                    // mismatch after j matches
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j-1];
+                else
+                    i = i+1;
+            }
+        }
+    }

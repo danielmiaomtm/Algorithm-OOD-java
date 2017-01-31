ba5b8b4ae89057a4f464462b7dae4ebbd76954ae
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-31 15:02:59 -0800

    Update LinkedIn pow(a,b) & sqrt(x).java

--- LinkedIn pow(a,b) & sqrt(x).java
+++ LinkedIn pow(a,b) & sqrt(x).java
@@ -47,3 +47,24 @@ public double myPow(double x, int n) {
 
 
 }
+
+//pow(x, n)   iteration
+if (n == 0) return 1;
+        if (x == 1) return 1;
+        if (x == -1) {
+            if (n % 2 == 0) return 1;
+            else return -1;
+        }
+        if (n == Integer.MIN_VALUE) return 0;
+        if (n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+        double ret = 1.0;
+        while (n > 0) {
+            if ((n & 1) != 0) 
+                ret *= x;
+            x = x * x;
+            n = n >> 1;
+        }
+        return ret;

b699338481765fde7ac9c15588181aadafa83365
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-31 22:22:02 -0800

    Update LinkedIn Permutations II.java

--- LinkedIn Permutations II.java
+++ LinkedIn Permutations II.java
@@ -37,3 +37,36 @@ public class Solution {
         
     }
 }
+
+
+
+
+
+
+
+public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        List<Integer> list = new ArrayList<>();
+        helper (result, list, nums, new boolean[nums.length]);
+        return result;
+    }
+    public void helper (List<List<Integer>> result, List<Integer> list, int[] nums, boolean[] visited) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            list.add(nums[i]);
+            helper(result, list, nums, visited);
+            list.remove(list.size() - 1);
+            visited[i] = false;
+        }
+    }

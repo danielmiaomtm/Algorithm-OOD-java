1659b7c1b5e2a7c17af92db2b17e4ee2be6d56db
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-01 10:59:32 -0800

    Update LinkedIn mid stack find middle.java

--- LinkedIn mid stack find middle.java
+++ LinkedIn mid stack find middle.java
@@ -20,82 +20,7 @@ package LinkedList;
 
 import java.util.List;
 
-class DLLNode {
-  DLLNode left;
-  DLLNode right;
-  int val;
-}
-
-public class StackToFindMiddle {
-
-        DLLNode head;
-        DLLNode middle;
-        int size = 0;
-        public void push(int val) {
-                size++;
-                if(head == null) {
-                        head = new DLLNode(val);
-                        middle = head;
-                }else {
-                        DLLNode node = new DLLNode(val);
-                        node.right = head;
-                        head.left = node;
-                        head=node;
-                        // if even, middle should move to left
-                        if(size % 2 == 0) {
-                                middle = middle.left;
-                        }
-                }
-        }
-
-        public int pop() {
-                if(head == null) {
-                        return -1;
-                }
-                size--;
-                int result = head.val;
-                // there is only one elelment, reset the head and middle
-                if(size == 0) {
-                        head = null;
-                        middle = null;
-                } else {
-                        head = head.right;
-                        head.left = null;
-                        if(size % 2 == 1) {
-                                middle = middle.right;
-                        }
-                }
-                return result;
-        }
-
-        public int findMiddle() {
-                return middle.val;
-        }
-
-
-        public void deleteMiddle() {
-                size--;
-                if(middle.left != null) {
-                        middle.left.right=middle.right;
-                }
-                if(middle.right != null) {
-                        middle.right.left=middle.left;
-                }
-                if(size%2 == 1) {
-                        middle=middle.right;
-                }else {
-                        middle=middle.left;
-                }
-        }
-}
-
-
-
-
-
-
-
-
+//我的方法
 class DLLNode{
 	DLLNode pre;
 	DLLNode next;
@@ -190,3 +115,87 @@ public int popMid () {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class DLLNode {
+  DLLNode left;
+  DLLNode right;
+  int val;
+}
+
+public class StackToFindMiddle {
+
+        DLLNode head;
+        DLLNode middle;
+        int size = 0;
+        public void push(int val) {
+                size++;
+                if(head == null) {
+                        head = new DLLNode(val);
+                        middle = head;
+                }else {
+                        DLLNode node = new DLLNode(val);
+                        node.right = head;
+                        head.left = node;
+                        head=node;
+                        // if even, middle should move to left
+                        if(size % 2 == 0) {
+                                middle = middle.left;
+                        }
+                }
+        }
+
+        public int pop() {
+                if(head == null) {
+                        return -1;
+                }
+                size--;
+                int result = head.val;
+                // there is only one elelment, reset the head and middle
+                if(size == 0) {
+                        head = null;
+                        middle = null;
+                } else {
+                        head = head.right;
+                        head.left = null;
+                        if(size % 2 == 1) {
+                                middle = middle.right;
+                        }
+                }
+                return result;
+        }
+
+        public int findMiddle() {
+                return middle.val;
+        }
+
+
+        public void deleteMiddle() {
+                size--;
+                if(middle.left != null) {
+                        middle.left.right=middle.right;
+                }
+                if(middle.right != null) {
+                        middle.right.left=middle.left;
+                }
+                if(size%2 == 1) {
+                        middle=middle.right;
+                }else {
+                        middle=middle.left;
+                }
+        }
+}
+

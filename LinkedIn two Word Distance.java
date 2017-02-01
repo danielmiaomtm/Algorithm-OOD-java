0300c3f51bb35639e83347e26a1d6a4f6c02f45b
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-31 20:36:20 -0800

    Update LinkedIn two Word Distance.java

--- LinkedIn two Word Distance.java
+++ LinkedIn two Word Distance.java
@@ -2,18 +2,18 @@
 // query many times
 public class WordDistance {
     private Map<String, List<Integer>> map;
-    
+    // store all the pos to map
     public WordDistance(String[] words) {
         map = new HashMap<String, List<Integer>>();
         for(int i = 0; i < words.length; i++) {
-            String w = words[i];
-            if(map.containsKey(w)) {
-                map.get(w).add(i);
+            List<Integer> list;
+            if(map.containsKey(words[i])) {
+                list = map.get(words[i]);
             } else {
-                List<Integer> list = new ArrayList<Integer>();
-                list.add(i);
-                map.put(w, list);
+                list = new ArrayList<Integer>();
             }
+            list.add(i);    
+            map.put(words[i], list);
         }
     }
     
@@ -95,3 +95,20 @@ public class Solution {
         return minLen == Integer.MAX_VALUE ? -1 : minLen;
     }
 }
+
+
+public int shortestWordDistance(String[] words, String word1, String word2) {
+        int idx1 = -1, idx2 = -1, distance = Integer.MAX_VALUE, turn = 0, inc = (word1.equals(word2) ? 1 : 0);
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word1) && turn % 2 == 0){
+                idx1 = i;
+                if(idx2 != -1) distance = Math.min(distance, idx1 - idx2);
+                turn += inc;
+            } else if(words[i].equals(word2)){
+                idx2 = i;
+                if(idx1 != -1) distance = Math.min(distance, idx2 - idx1);
+                turn += inc;
+            }
+        }
+        return distance;
+    }

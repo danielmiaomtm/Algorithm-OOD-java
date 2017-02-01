ae01cd54325a413676a8d6eb74d665ddb4322c48
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-01-31 20:31:50 -0800

    Update and rename LinkedIn Shortest Word Distance.java to LinkedIn two Word Distance.java

--- LinkedIn two Word Distance.java
+++ LinkedIn two Word Distance.java
@@ -61,7 +61,7 @@ public class Solution {
 }
 
 
-
+// if two words could be same
 public class Solution {
     public int shortestWordDistance(String[] words, String word1, String word2) {
         boolean appeared = false;

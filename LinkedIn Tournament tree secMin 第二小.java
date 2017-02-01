27df35a42d3f9e1c8eea7f8fecc033fc616ae342
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-01 10:32:10 -0800

    Update LinkedIn Tournament tree secMin 第二小.java

--- LinkedIn Tournament tree secMin 第二小.java
+++ LinkedIn Tournament tree secMin 第二小.java
@@ -1,14 +1,19 @@
 /*
-ournament tree 找secMin;. visit 1point3acres.com for more.
+ournament tree 找secMin;
 
-Tournament tree 的定义是parent 是孩子node的最小值， 如下例 return 5
+Tournament tree 的定义是parent 是孩子node的最小值， 如下例 return 3
 
                 2
               /  \
             2     7
-          /  \    | \
-        5    2    8  7
-
+             \   / \
+             2   7  8
+            / \
+           3   2 
+        1.先走到底，如果是leaf返回，如果是null返回最大值，
+        2.left,right 返回的时候看返回值是不是和最小值，是的话就将值设为最大值
+        3.最后返回left,right最小值
+        
 */
 
 
@@ -18,32 +23,26 @@ public int secondMin (tournamentTree root) {
     return -1;
   }
   int result = helper(root, root.val);
+  //如果全部都是最小值那就返回-1，找不到
   return result == Integer.MAX_VALUE ? -1 : result;
-  }
+}
 
 public int helper (tournamentTree root, int minVal) {
     if (root == null) {
       return Integer.MAX_VALUE;
-  }
-  if (root.left == null && root.right == null) {
-    return root.val;
-  }
-  int left = helper(root.left, minVal);
-  int right = helper(root.right, minVal);
+    }
+    if (root.left == null && root.right == null) {
+      return root.val;
+    }
+    int left = helper(root.left, minVal);
+    int right = helper(root.right, minVal);
 
-  if (left == minVal) {
-    left = Integer.MAX_VALUE;
-  }
-  if (right == minVal) {
-    right = Integer.MAX_VALUE;
-  }
+    if (left == minVal) {
+      left = Integer.MAX_VALUE;
+    }
+    if (right == minVal) {
+      right = Integer.MAX_VALUE;
+    }
 
-  if (left == minVal) {
-    return right;
-  } else if (right == minVal) {
-    return left;
-  } else {
     return Math.min(left, right);
-  }
-
 }

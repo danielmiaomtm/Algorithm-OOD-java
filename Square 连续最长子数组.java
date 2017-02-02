d73a06f3d0f1ea5599772c285b35ec577e25e098
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-01 23:04:13 -0800

    Create Square 连续最长子数组.java

---
+++ Square 连续最长子数组.java
@@ -0,0 +1,17 @@
+/*
+1. 给一个数组，找出一个长度最长的子数组，里面的值是连续的。 
+Follow up: 连续的值可以跳着的，不一定index也是连续，返回这种子序列的最大长度。 
+
+*/
+
+
+public List<Integer> maxContinList (int[] nums) {
+  //two pointer start end, to get the result
+}
+
+
+follow up:
+
+int upper = -1
+int lower = -1;
+maxLen = Math.max(maxLen, upper - lower);

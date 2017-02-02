93d0878d10b6176268adde5c2850522ab2a5a9e9
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-01 22:03:53 -0800

    Create Square primes.java

---
+++ Square primes.java
@@ -0,0 +1,29 @@
+public List<Integer> printPrime (int n) {
+    List<Integer> result = new ArrayList<>();
+    if (n < 2) {
+      return result;
+    }
+
+    boolean[] primes = new boolean[n + 1];
+    Arrays.fill(primes, false);
+    for (int i = 2; i <= n; i++) {
+      if (primes[i]) {
+        continue;
+      }
+      for (int j = i * 2; j <= n; j += i) {
+        primes[j] = true;
+      }
+    }
+
+    for (int i = 2; i <= n; i++) {
+
+      if (!primes[i]) {
+        result.add(i);
+      }
+    }
+    return result;
+  }
+
+
+
+

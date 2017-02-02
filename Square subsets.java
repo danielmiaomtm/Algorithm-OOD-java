13ebafa912b29e145572edcf673c03b8902320e4
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-01 18:51:17 -0800

    Create Square subsets.java

---
+++ Square subsets.java
@@ -0,0 +1,24 @@
+	public List<List<Integer>> subsets (List<Integer> input) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (input == null || input.size() == 0) {
+			return result;
+		}
+		List<Integer> list = new ArrayList<>();
+		Collections.sort(input);
+		helper(input, result, list, 0);
+		return result;
+	}
+	public void helper (List<Integer> input, List<List<Integer>> result, List<Integer> list, int index) {
+		result.add(new ArrayList<>(list));
+
+		for (int i = index; i < input.size(); i++) {
+			// if there is duplicates one, skip
+			if (i != index && input.get(i) == input.get(i - 1)) {
+				continue;
+			}
+			list.add(input.get(i));
+			helper (input, result, list, i + 1);
+			list.remove(list.size() - 1);
+		}
+
+	}

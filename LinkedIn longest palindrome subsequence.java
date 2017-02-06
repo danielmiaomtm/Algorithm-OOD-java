e28363886f5f1ea0d1f7641bed421c0ebc5976a3
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-06 11:09:14 -0800

    Update LinkedIn longest palindrome subsequence.java

--- LinkedIn longest palindrome subsequence.java
+++ LinkedIn longest palindrome subsequence.java
@@ -12,25 +12,29 @@
  
   */
   
-  public int calculate1(char []str){
-        int T[][] = new int[str.length][str.length];
-        for(int i=0; i < str.length; i++){
-            T[i][i] = 1;
-        }
-        for(int l = 2; l <= str.length; l++){
-            for(int i = 0; i < str.length-l + 1; i++){
-                int j = i + l - 1;
-                if(l == 2 && str[i] == str[j]){
-                    T[i][j] = 2;
-                }else if(str[i] == str[j]){
-                    T[i][j] = T[i + 1][j-1] + 2;
-                }else{
-                    T[i][j] = Math.max(T[i + 1][j], T[i][j - 1]);
-                }
-            }
-        }
-        return T[0][str.length-1];
-    }
+public int calculate1(char[] str){
+      int[][] T = new int[str.length][str.length];
+      for(int i = 0; i < str.length; i++){
+          T[i][i] = 1;
+      }
+
+      for(int col = 1; col < str.length; col++){
+          for(int row = 0; row < str.length - col; row++){
+              // new col
+            int j = row + col;
+              if(j == 1 && str[row] == str[j]){
+                  T[row][j] = 2;
+              }else if(str[row] == str[j]){
+                  T[row][j] = T[row + 1][j - 1] + 2;
+              }else{
+                  T[row][j] = Math.max(T[row + 1][j], T[row][j - 1]);
+              }
+          }
+      }
+
+
+      return T[0][str.length-1];
+  }
 
 
     public int calculateRecursive(char str[],int start,int len){

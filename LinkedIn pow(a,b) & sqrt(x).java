f416f4ee1bc2ab0c6ea48ee573db366f85fd1e83
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-08 12:04:13 -0800

    Update LinkedIn pow(a,b) & sqrt(x).java

--- LinkedIn pow(a,b) & sqrt(x).java
+++ LinkedIn pow(a,b) & sqrt(x).java
@@ -28,6 +28,25 @@ public int mySqrt(int x) {
 
 
 
+public static double mySqrt(double x, int digit) {
+		double start = 0;
+		double end = x < 1 ? 1.0 : x;
+		double diff = 1 / Math.pow(10, digit);
+		while (start < end - diff) {
+			double mid = start + (end - start) / 2;
+			if (mid == x / mid) {
+				return mid;
+			} else if (mid > x / mid) {
+				end = mid;
+			} else {
+				start = mid;
+			}
+		}
+		return (start + end) / 2;
+	}
+
+
+
 
 
 

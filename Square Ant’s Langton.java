b03b024cb4a6968387debe4561509cb1f0174873
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-10 11:19:03 -0800

    Update Square Ant’s Langton.java

--- Square Ant’s Langton.java
+++ Square Ant’s Langton.java
@@ -17,9 +17,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[][] board = new int[][]{{1,0,0},
-				 				     {0,0,0}, 
-				 				     {1,1,1},
-				 				     {0,0,0}};  
+				    {0,0,0}, 
+				    {1,1,1},
+				    {0,0,0}};  
 		 
 		 sol.move(1, 1, 2, board, 5);      
 	     for (int[] b : board) {

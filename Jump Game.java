c218a5d33cd5a7d1d45d24f22c30032246b44c65
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-02-20 11:24:27 -0800

    Update Jump Game.java

--- Jump Game.java
+++ Jump Game.java
@@ -31,3 +31,40 @@ For example, given array A = [2,3,1,1,4], the minimum number of jumps to reach t
 
       return step;
     }
+
+
+
+
+	public int jump(int[] nums) {
+
+        int step = 0;
+        int curMaxArea = 0;
+        int maxNext = 0;
+        
+        for(int i = 0; i < nums.length - 1; i++) {
+            maxNext = Math.max(maxNext, i+nums[i]);
+            if( i == curMaxArea ) {
+                step++;
+                curMaxArea = maxNext;
+            } 
+        }
+        return curMaxArea >= nums.length - 1 ? step : 0;
+    }
+	
+//打印出path
+    public static List<String> helper(int[] nums, int step){
+		List<String> res=new ArrayList<>();
+	    dfs(res,nums,"",step,0);
+	    return res;
+	}
+	public static void dfs(List<String> res, int[] nums, String s, int level, int start){
+	    if (level < 0) return;
+	    s = s + start;
+	    if(level == 0 && start == nums.length - 1){
+	        res.add(s);
+	        return;
+	    }
+	    for(int i = 1; i <= nums[start]; i++){
+	        dfs(res,nums,s,level - 1,start + i);
+	    }
+	}

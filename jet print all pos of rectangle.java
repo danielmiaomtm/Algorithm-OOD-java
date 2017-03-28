22213623bb5b076eda9c4443832f37c19fbc4b7e
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-03-27 17:22:39 -0700

    Update jet print all pos of rectangle.java

--- jet print all pos of rectangle.java
+++ jet print all pos of rectangle.java
@@ -46,3 +46,56 @@ public List<List<Integer>> triPos (int[][] matrix) {
 		    }
 		    
 		}
+
+
+
+//如果原数组不能被改变	
+public List<List<Integer>> triPos (int[][] matrix) {
+	    
+		List<List<Integer>> result = new ArrayList<>();
+		Set<Integer> visited = new HashSet<>();  
+	    int rowNum = matrix.length, colNum = matrix[0].length;
+	    
+	    for (int i = 0; i < rowNum; i++) {
+	      for (int j = 0; j < colNum; j++) {
+	        if (!visited.contains(i * colNum + j) && matrix[i][j] == 0) {
+	          List<Integer> list = new ArrayList<>();
+	          list.add(i);
+	          list.add(j);
+	          helper(matrix, list, i, j, rowNum, colNum, visited);
+	          result.add(new ArrayList<>(list));
+	        }
+	      }
+	    }
+	  
+	  
+	  return result;
+	}
+
+	public void helper (int[][] matrix, List<Integer> list, int i, int j, int rowNum, int colNum, Set<Integer> visited) {
+	   
+	    // go to right, find width
+	    int width = 0;
+	    int row = i, col = j;
+	    while (col < colNum && matrix[i][col] == 0) {
+	      width++; 
+	      col++;
+	    }
+	    // go to bottom, find height  
+	    int height = 0;
+	    while (row < rowNum && matrix[row][j] == 0) {
+	      height++;
+	      row++;
+	    }
+	    
+	    list.add(width);
+	    list.add(height);
+	    
+	    //update the triangle area from 0 to 1
+	    for (row = 0; row < height; row++) {
+	    	for (col = 0; col < width; col++) {
+	    		visited.add((row + i) * colNum + (j + col));;
+	    	}
+	    }
+	    
+	}

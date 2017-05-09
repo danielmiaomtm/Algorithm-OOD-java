9bc2c10e3c87fa33a5455ee0e624362b9478f920
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-05-09 16:01:04 -0700

    Update LinkedIn H2O.java

--- LinkedIn H2O.java
+++ LinkedIn H2O.java
@@ -1,4 +1,43 @@
 public class H2O {
+ 
+  static final int H = 0;
+  static final int O = 1;
+  static final Lock LOCK = new ReentrantLock();
+  static final Condition ENOUGH[] = {LOCK.newCondition(), LOCK.newCondition()};
+  static final int NEEDED[] = {2, 1};
+  static final int COUNT[] = {0, 0};
+ 
+  static void run(int index) {
+    LOCK.lock();
+    try {
+      if(COUNT[H] >= NEEDED[H] && COUNT[O] >= NEEDED[O]) {
+        for(int i = 0, n = NEEDED[H]; i < n; ++i) {
+          ENOUGH[H].signal();
+        }
+        COUNT[H] -= NEEDED[H];
+        for(int i = 0, n = NEEDED[O]; i < n; ++i) {
+          ENOUGH[O].signal();
+        }
+        COUNT[O] -= NEEDED[O];
+      }
+      ++COUNT[index];
+      ENOUGH[index].awaitUninterruptibly();
+    } finally {
+      LOCK.unlock();
+    }
+  }
+ 
+  public static void h() {
+    run(H);
+  }
+ 
+  public static void o() {
+    run(O);
+  }
+}
+
+
+public class H2O {
 
 
     static final Lock LOCK = new ReentrantLock();

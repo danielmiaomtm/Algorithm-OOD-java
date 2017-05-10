cdfc33169fc567c80e82cf68a1c69d5b33cbfe48
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-05-10 16:02:18 -0700

    Update LinkedIn Tournament tree secMin 第二小.java

--- LinkedIn Tournament tree secMin 第二小.java
+++ LinkedIn Tournament tree secMin 第二小.java
@@ -16,8 +16,33 @@ Tournament tree 的定义是parent 是孩子node的最小值， 如下例 return
         
 */
 
+//Method 1
+public static int secMin(TreeNode root) {
+      if (root == null || (root.left == null && root.right == null)) {
+              return -1;
+      }
+      int left = -1, right = -1;
+      if (root.left.val == root.val) {
+              left = secMin(root.left);
+      } else {
+              left = root.left.val;
+      }
+      if (root.right.val == root.val) {
+              right = secMin(root.right);
+      } else {
+              right = root.right.val;
+      }
+      if (left == -1 || right == -1) {
+              return left == -1 ? right : left;
+      }
+      return Math.min(left, right);
+  }
 
 
+
+
+
+//Method 2
 public int secondMin (tournamentTree root) {
   if (root == null || (root.left == null && root.right == null) || root.left == null || root.right == null) {
     return -1;

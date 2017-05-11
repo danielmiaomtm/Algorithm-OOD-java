2247a64259e07e8a7fac1beec200fb73433654f0
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-05-10 17:01:30 -0700

    Update LinkedIn Can I win.java

--- LinkedIn Can I win.java
+++ LinkedIn Can I win.java
@@ -38,24 +38,27 @@ Show Similar Problems
 
 // memo O(2^n) if not (n!)
 public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        if (desiredTotal <= 0) return true;
-        if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal) return false;
-        return canIWin(desiredTotal, new int[maxChoosableInteger], new HashMap<>());
+        if (desiredTotal<=0) return true;
+        //如果1到最大能选的值所有和都不能满足目标值，那么肯定失败
+        if (maxChoosableInteger*(maxChoosableInteger+1)/2<desiredTotal) return false;
+        char state[] = new char[maxChoosableInteger];
+        for(int i=0;i<maxChoosableInteger;i++) state[i] = '0';
+        return dfs(desiredTotal, state, new HashMap<>());
     }
-    private boolean canIWin(int leftTotal, int[] state, HashMap<String, Boolean> hashMap) {
-        String curr = Arrays.toString(state);
-        if (hashMap.containsKey(curr)) return hashMap.get(curr);
-        for (int i = 0;i < state.length; i++) {
-            if (state[i] == 0) {
-                state[i] = 1;
-                if (leftTotal <= i + 1 || !canIWin(leftTotal - (i + 1), state, hashMap)) {
-                    hashMap.put(curr, true);
-                    state[i] = 0;
+    private boolean dfs(int total, char[] state, HashMap<String, Boolean> hashMap) {
+        String key= new String(state);
+        if (hashMap.containsKey(key)) return hashMap.get(key);
+        for (int i=0;i<state.length;i++) {
+            if (state[i]=='0') {
+                state[i]='1';
+                if (total<=i+1 || !dfs(total-(i+1), state, hashMap)) {
+                    hashMap.put(key, true);
+                    state[i]='0';
                     return true;
                 }
-                state[i] = 0;
+                state[i]='0';
             }
         }
-        hashMap.put(curr, false);
+        hashMap.put(key, false);
         return false;
     }

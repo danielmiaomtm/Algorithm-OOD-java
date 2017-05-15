0cf2d9da2cf661666b76f0c3437fca4c5d2ae029
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-05-14 17:07:06 -0700

    Create PocketGem strStr.java

---
+++ PocketGem strStr.java
@@ -0,0 +1,8 @@
+public int strStr(String s, String t) {
+        if (t.isEmpty()) return 0; // edge case: "",""=>0  "a",""=>0
+        for (int i = 0; i <= s.length() - t.length(); i++) {
+            for (int j = 0; j < t.length() && s.charAt(i + j) == t.charAt(j); j++)
+                if (j == t.length() - 1) return i;
+        }
+        return -1;
+    }

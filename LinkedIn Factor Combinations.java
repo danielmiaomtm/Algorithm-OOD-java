ae80ab241fadf486f5670f4a71ffad8fb2d56db0
danielmiaomtm <jeffreymaomtm@gmail.com>
2017-05-18 15:24:04 -0700

    Update LinkedIn Factor Combinations.java

--- LinkedIn Factor Combinations.java
+++ LinkedIn Factor Combinations.java
@@ -61,3 +61,40 @@ public class Solution {
         
     }
 }
+
+
+
+
+/*
+Print all unique combinations of factors of a positive integer. For example given 24:
+
+24*1
+12*2
+8*3
+6*4
+6*2*2
+4*3*2
+3*2*2*2
+*/
+
+public List<List<Integer>> factorCombinations(int n) {
+	    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+	    helper(ans, n, n / 2, new ArrayList<Integer>());
+	    return ans;
+	}
+
+	private void helper(List<List<Integer>> ans, int num, int largestFactor,
+	        List<Integer> path) {
+	    if (num == 1) {
+	        ans.add(new ArrayList<Integer>(path));
+	        return;
+	    }
+	    for (int i = largestFactor; i > 1; i--) {
+	        if (num % i == 0) {
+	            path.add(i);
+	            helper(ans, num / i, i, path);
+	            path.remove(path.size() - 1);
+	        }
+	    }
+	}
+}
